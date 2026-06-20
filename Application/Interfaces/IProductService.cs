5c78cabc50ae8409e8278fc8289bdd25e6a4378a
Prachi Nadgir <nadgirprachi@gmail.com>
2026-06-20 09:16:30 +0530

    Add IProductService interface with async CRUD methods
    Introduced IProductService in Application.Interfaces, defining asynchronous CRUD operations for products using DTOs for input and output. Methods include GetAllAsync, GetByIdAsync, CreateAsync, UpdateAsync, and DeleteAsync.

---
+++ Application/Interfaces/IProductService.cs
@@ -0,0 +1,21 @@
+using Application.DTOs;
+
+namespace Application.Interfaces;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductResponseDto>>
+        GetAllAsync();
+
+    Task<ProductResponseDto?>
+        GetByIdAsync(int id);
+
+    Task<ProductResponseDto>
+        CreateAsync(CreateProductDto dto);
+
+    Task UpdateAsync(
+        int id,
+        UpdateProductDto dto);
+
+    Task DeleteAsync(int id);
+}
